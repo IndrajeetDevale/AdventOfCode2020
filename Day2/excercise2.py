17eee2c2f407e40402835d0bea4b368b050c4c3b
IndrajeetDevale <indrajeet.devale@gmail.com>
2020-12-02 22:54:59 +0530

    Solution for day2. Will be optimized whenever possible. Feedback appreciated

---
+++ Day2/excercise2.py
@@ -0,0 +1,35 @@
+import re
+
+file_object = open(r"input.txt","r")
+
+count =0
+valid = 0
+valid1 = 0
+num1 = 0
+num2 = 0
+
+#split password string
+def split(inputstring):
+	return[char for char in inputstring]
+
+
+for i in file_object.read().strip().splitlines():
+	#split data info
+	my_list = re.split('[ :-]',i)
+	a = split(my_list[4])
+	#part1
+	for j in a:
+		if my_list[2] == j:
+			count +=1
+	if int(my_list[0]) <= count <= int(my_list[1]):
+		valid += 1
+	count = 0
+
+	#part2
+	if (my_list[2] == a[int(my_list[0])-1]) ^ (my_list[2] == a[int(my_list[1])-1]):
+		valid1 +=1
+
+print(valid)
+print(valid1)
+
+
